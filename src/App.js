1ec078be803184c3eeca48f4de44f897ecfca264
manukempo <manukempo@gmail.com>
2017-08-24 19:18:39 +0100

    Components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// CLASS METHOD
+// class App extends React.Component{
+//   render(){
+//     return React.createElement('h1', null, 'Hello Hello');
+//   }
+
+// }
+
+// STATELESS FUNCTION
+const App = () => <h1>Hello World again!</h1>
+export default App;
