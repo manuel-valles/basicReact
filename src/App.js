240085aaec5788fb2602bf0b62cfb973fb7860cf
manukempo <manukempo@gmail.com>
2017-08-24 20:14:22 +0100

    Component State

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component{
-  render(){
-    let txt = this.props.txt;
-    return (
-    	<div>
-    		<h1>Hello World!</h1> 
-    		<b>Bold</b>
-    		<p>{txt}</p>
-    	</div>
-    );
-  }
+	constructor(){
+		super();
+		this.state = {
+			txt: 'this is the state txt',
+			cat: 0
+		}
+	}
+	update(e){
+		this.setState({txt: e.target.value});
+	}
+	render(){
+		let txt = this.props.txt;
+		return (
+			<div>
+				<h1>Hello World!</h1> 
+				<b>Bold</b>
+				<p>{txt}</p>
+				<p>{this.state.txt} - {this.state.cat}</p>
+				<input type="text" onChange={this.update.bind(this)}/>
+			</div>
+		);
+	}
 }
 
 App.propTypes={
