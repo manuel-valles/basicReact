4bda4df14c091eeea396004ad96a992574821a7f
manukempo <manukempo@gmail.com>
2017-08-27 19:42:47 +0100

    Children utilities

--- src/App2.js
+++ src/App2.js
@@ -1,44 +1,29 @@
 import React from 'react';
-import './App.css';
 
 class App2 extends React.Component{
-	constructor(){
-		super();
-		this.state={
-			input: '/* add your jsx here */',
-			output: '',
-			err: ''
-		}
-	}
-	update(e){
-		let code = e.target.value;
-		try{
-			this.setState({
-				output: window.Babel
-				.transform(code, {presets: ['es2015', 'react']})
-				.code,
-				err: ''
-			})
-		}
-		catch(err){
-			this.setState({err: err.message})
-		}
-	}
 	render(){
 		return(
-			<div>
-				<header>{this.state.err}</header>
-				<div className="container">
-					<textarea
-						onChange={this.update.bind(this)}
-						defaultValue={this.state.input}
-					/>
-					<pre>
-						{this.state.output}
-					</pre>
-				</div>
-			</div>
+			<Parent>
+				<div className="childA"></div>
+
+			</Parent>
 		)
+	}
+}
+
+class Parent extends React.Component{
+	render(){
+		let items1=React.Children
+		.map(this.props.children, child=>child);
+
+		let items2=React.Children
+				.forEach(this.props.children, 
+					child => console.log(child.props.className));
+
+		let items3=React.Children
+			.only(this.props.children);
+		console.log(items1, items2, items3);
+		return null;
 	}
 }
 
